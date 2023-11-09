2b359319ef5047ec0dc06928544f2fbccec42c6f
Abdul Rafey <csyedabdulrafeyali@gmail.com>
2023-11-09 11:01:33 +0500

    Logging Requests with Morgan

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan");
 
 const planetsRouter = require("./routes/planets/planets.router");
 
@@ -13,6 +14,8 @@ app.use(
     origin: "http://localhost:3000",
   })
 );
+app.use(morgan("combined"));
+
 app.use(express.json());
 app.use(planetsRouter);
 
