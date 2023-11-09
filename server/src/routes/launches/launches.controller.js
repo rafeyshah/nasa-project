a68b30b83254f7d68acad21d7f76bcedc4d4bc2f
Abdul Rafey <csyedabdulrafeyali@gmail.com>
2023-11-09 15:19:17 +0500

    POST /launches: validation for post requests

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -7,7 +7,22 @@ const httpGetAllLaunches = (req, res) => {
 const httpAddNewLaunch = (req, res) => {
   const launch = req.body;
 
+  if (
+    !launch.mission ||
+    !launch.rocket ||
+    !launch.launchDate ||
+    !launch.destination
+  ) {
+    return res.status(400).json({
+      error: "Missing required launch property",
+    });
+  }
   launch.launchDate = new Date(launch.launchDate);
+  if (isNaN(launch.launchDate.valueOf())) {
+    return res.status(400).json({
+      error: "Invalid launch date",
+    });
+  }
 
   addNewLaunch(launch);
   return res.status(201).json(launch);
