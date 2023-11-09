ff6aaff43aee2d49eaf66d1e8a270707664d2543
Abdul Rafey <csyedabdulrafeyali@gmail.com>
2023-11-09 12:32:44 +0500

    Get /launches

--- server/src/server.js
+++ server/src/server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const morgan = require("morgan");
 
 const planetsRouter = require("./routes/planets/planets.router");
+const launchesRouter = require("./routes/launches/launches.router");
 
 const { loadPlanetsData } = require("./models/planets.model");
 
@@ -18,6 +19,7 @@ app.use(morgan("combined"));
 
 app.use(express.json());
 app.use(planetsRouter);
+app.use(launchesRouter);
 
 const PORT = process.env.PORT || 8000;
 
