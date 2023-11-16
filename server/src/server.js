05a3684a2545e856c41248d6e06453ccbf14ce61
Abdul Rafey <csyedabdulrafeyali@gmail.com>
2023-11-16 12:55:59 +0500

    Versioning Node APIs

--- server/src/server.js
+++ server/src/server.js
@@ -5,8 +5,7 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 
-const planetsRouter = require("./routes/planets/planets.router");
-const launchesRouter = require("./routes/launches/launches.router");
+const apiRouter = require("./routes/api");
 
 const { loadPlanetsData } = require("./models/planets.model");
 
@@ -20,13 +19,12 @@ app.use(
 );
 app.use(morgan("combined"));
 app.use(express.json());
+app.use("/v1", apiRouter);
 
 exports.app = app;
 
-app.use("/planets", planetsRouter);
-app.use("/launches", launchesRouter);
-
-const PORT = process.env.PORT || 8000;
+// const PORT = process.env.PORT || 8000;
+const PORT = 8000;
 
 const server = http.createServer(app);
 
